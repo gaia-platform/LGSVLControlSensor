10542be4e959cabdc7a01fc5b5f2e30decb6edf7
Hadi Tabatabaee <hadi.tabatabaee@lge.com>
2021-02-11 13:50:24 -0800

    Only process lgsvl control messages

--- LGSVLControlSensor.cs
+++ LGSVLControlSensor.cs
@@ -44,19 +44,8 @@ namespace Simulator.Sensors
         public AnimationCurve AccelerationInputCurve;
         public AnimationCurve BrakeInputCurve;
 
-        double LastTimeStamp = 0;  // from Apollo
-
         VehicleControlData controlData;
 
-        private enum ControlType
-        {
-            None,
-            AutowareAi,
-            Apollo,
-            LGSVL,
-            AutowareAuto,
-        };
-        ControlType controlType = ControlType.None;
 
         private void Awake()
         {
@@ -99,95 +88,12 @@ namespace Simulator.Sensors
                 controlData = data;
                 LastControlUpdate = SimulatorManager.Instance.CurrentTime;
 
-                if (data.Velocity.HasValue) // autoware
-                {
-                    controlType = ControlType.AutowareAi;
-                    if (data.ShiftGearUp || data.ShiftGearDown)
-                    {
-                        if (data.ShiftGearUp) Dynamics.GearboxShiftUp();
-                        if (data.ShiftGearDown) Dynamics.GearboxShiftDown();
+                float wheelAngle = data.SteerAngle.GetValueOrDefault();
+                wheelAngle = UnityEngine.Mathf.Clamp(wheelAngle, -Dynamics.MaxSteeringAngle, Dynamics.MaxSteeringAngle);
+                var k = (float)(wheelAngle + Dynamics.MaxSteeringAngle) / (Dynamics.MaxSteeringAngle * 2);
 
-                        float wheelAngle = data.SteerAngle.GetValueOrDefault();
-                        wheelAngle = UnityEngine.Mathf.Clamp(wheelAngle, -Dynamics.MaxSteeringAngle, Dynamics.MaxSteeringAngle);
-                        var k = (float)(wheelAngle + Dynamics.MaxSteeringAngle) / (Dynamics.MaxSteeringAngle * 2);
-                        ADSteerInput = UnityEngine.Mathf.Lerp(-1f, 1f, k);
-
-                        // This method of control is not accurate because of the hardcoded numbers
-                        // For accurate control an external controller should be used with the Lgsvl control type
-                        float MaxAcceleration = 5.5f;  // m/s^2
-                        float MaxDeceleration = -9f;  // m/s^2
-                        var NetAcceleration = data.Acceleration.GetValueOrDefault() - data.Braking.GetValueOrDefault();
-                        NetAcceleration = UnityEngine.Mathf.Clamp(NetAcceleration, MaxDeceleration, MaxAcceleration);
-                        if (NetAcceleration > 0)
-                        {
-                            ADAccelInput = NetAcceleration / MaxAcceleration;
-                        }
-                        else
-                        {
-                            ADAccelInput = NetAcceleration / MaxDeceleration;
-                        }
-                    }
-                    else
-                    {
-                        if (Dynamics.Reverse) return; // TODO move?
-
-                        var linMag = Mathf.Clamp(Mathf.Abs(data.Velocity.GetValueOrDefault() - ActualLinVel), 0f, 1f);
-                        ADAccelInput = ActualLinVel < data.Velocity.GetValueOrDefault() ? linMag : -linMag;
-                        ADSteerInput = -Mathf.Clamp(data.SteerAngularVelocity.GetValueOrDefault() * 0.5f, -1f, 1f);
-                    }
-                }
-                else if (data.SteerRate.HasValue) // apollo
-                {
-                    if (double.IsInfinity(data.Acceleration.GetValueOrDefault()) || double.IsInfinity(data.Braking.GetValueOrDefault()) ||
-                        double.IsNaN(data.Acceleration.GetValueOrDefault()) || double.IsNaN(data.Braking.GetValueOrDefault()))
-                    {
-                        return;
-                    }
-
-                    controlType = ControlType.Apollo;
-                    var timeStamp = data.TimeStampSec.GetValueOrDefault();
-                    var dt = (float)(timeStamp - LastTimeStamp);
-                    LastTimeStamp = timeStamp;
-
-                    Debug.Assert(data.Acceleration.GetValueOrDefault() >= 0 && data.Acceleration.GetValueOrDefault() <= 1);
-                    Debug.Assert(data.Braking.GetValueOrDefault() >= 0 && data.Braking.GetValueOrDefault() <= 1);
-                    var linearAccel = AccelerationInputCurve.Evaluate(data.Acceleration.GetValueOrDefault()) - BrakeInputCurve.Evaluate(data.Braking.GetValueOrDefault());
-
-                    var steeringTarget = -data.SteerTarget.GetValueOrDefault();
-                    var steeringAngle = Controller.SteerInput;
-                    var sgn = Mathf.Sign(steeringTarget - steeringAngle);
-                    var steeringRate = data.SteerRate.GetValueOrDefault() * sgn;
-                    steeringAngle += steeringRate * dt;
-
-                    if (sgn != steeringTarget - steeringAngle) // to prevent oversteering
-                        steeringAngle = steeringTarget;
-
-                    ADSteerInput = steeringAngle;
-                    ADAccelInput = linearAccel;
-
-                    if (data.CurrentGear == GearPosition.Reverse)
-                    {
-                        Dynamics.ShiftReverseAutoGearBox();
-                    }
-                    else if (data.CurrentGear == GearPosition.Drive)
-                    {
-                        Dynamics.ShiftFirstGear();
-                    }
-                }
-                else if (data.Acceleration.HasValue) // lgsvl
-                {
-                    controlType = ControlType.LGSVL;
-                    float wheelAngle = data.SteerAngle.GetValueOrDefault();
-                    wheelAngle = UnityEngine.Mathf.Clamp(wheelAngle, -Dynamics.MaxSteeringAngle, Dynamics.MaxSteeringAngle);
-                    var k = (float)(wheelAngle + Dynamics.MaxSteeringAngle) / (Dynamics.MaxSteeringAngle * 2);
-
-                    ADSteerInput = UnityEngine.Mathf.Lerp(-1f, 1f, k);
-                    ADAccelInput = data.Acceleration.GetValueOrDefault() - data.Braking.GetValueOrDefault();
-                }
-                else
-                {
-                    controlType = ControlType.None;
-                }
+                ADSteerInput = UnityEngine.Mathf.Lerp(-1f, 1f, k);
+                ADAccelInput = data.Acceleration.GetValueOrDefault() - data.Braking.GetValueOrDefault();
             });
         }
 
@@ -196,7 +102,6 @@ namespace Simulator.Sensors
             Debug.Assert(visualizer != null);
             var graphData = new Dictionary<string, object>()
             {
-                {"Control Type", controlType},
                 {"AD Accel Input", ADAccelInput},
                 {"AD Steer Input", ADSteerInput},
                 {"Last Control Update", LastControlUpdate},
@@ -204,47 +109,13 @@ namespace Simulator.Sensors
                 {"Actual Angular Velocity", ActualAngVel},
             };
 
-            switch (controlType)
+            if (controlData == null)
             {
-                case ControlType.None:
-                    break;
-                case ControlType.AutowareAi:
-                    if (controlData == null)
-                    {
-                        return;
-                    }
-                    graphData.Add("Shift Up", controlData.ShiftGearUp);
-                    graphData.Add("Shift Down", controlData.ShiftGearDown);
-                    graphData.Add("Acceleration", controlData.Acceleration.GetValueOrDefault());
-                    graphData.Add("Braking", controlData.Braking.GetValueOrDefault());
-                    graphData.Add("Steer Angle", controlData.SteerAngle.GetValueOrDefault());
-                    graphData.Add("Velocity", controlData.Velocity.GetValueOrDefault());
-                    graphData.Add("Steer Angle Velocity", controlData.SteerAngularVelocity.GetValueOrDefault());
-                    break;
-                case ControlType.LGSVL:
-                    if (controlData == null)
-                    {
-                        return;
-                    }
-                    graphData.Add("Acceleration", controlData.Acceleration.GetValueOrDefault());
-                    graphData.Add("Braking", controlData.Braking.GetValueOrDefault());
-                    graphData.Add("Steer Angle", controlData.SteerAngle.GetValueOrDefault());
-                    break;
-                case ControlType.Apollo:
-                    if (controlData == null)
-                    {
-                        return;
-                    }
-                    graphData.Add("Acceleration", controlData.Acceleration.GetValueOrDefault());
-                    graphData.Add("Braking", controlData.Braking.GetValueOrDefault());
-                    graphData.Add("Time Stamp Sec", controlData.TimeStampSec.GetValueOrDefault());
-                    graphData.Add("Steer Rate", controlData.SteerRate.GetValueOrDefault());
-                    graphData.Add("Steer Target", controlData.SteerTarget.GetValueOrDefault());
-                    graphData.Add("Gear", controlData.CurrentGear);
-                    break;
-                default:
-                    break;
+                return;
             }
+            graphData.Add("Acceleration", controlData.Acceleration.GetValueOrDefault());
+            graphData.Add("Braking", controlData.Braking.GetValueOrDefault());
+            graphData.Add("Steer Angle", controlData.SteerAngle.GetValueOrDefault());
 
             visualizer.UpdateGraphValues(graphData);
         }
